e0aa7e5b2b6543899e1e970ca45baa50ed695310
gpatasha <patashuri2003@gmail.com>
2025-01-22 21:47:24 +0400

    added animations

---
+++ src/components/ui/circles/Circles.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Circles.scss";
+function Circles() {
+  return (
+    <div className="circles">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Circles;
