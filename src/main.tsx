150eb941a592cf5322919714b84918b5f0df4821
GIORGI-PC <patashuri2003@gmail.com>
2025-01-23 08:13:47 +0400

    changed routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,29 +5,40 @@ import "./index.css";
 import EditResume from "./pages/resume/index.tsx";
 import ResumePreview from "./pages/resume/components/ResumePreview.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ResumeInfoContext } from "./context/ResumeInfoContext.tsx";
-import MyResume from "./pages/MyResume.tsx";
+import {
+  ResumeInfoContext,
+  ResumeInfoProvider,
+} from "./context/ResumeInfoContext.tsx";
 import JobVacancies from "./pages/JobVacancies.tsx";
-import Login from "./pages/Login.tsx";
-import Registration from "./pages/Registration.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthLayout } from "./components/AuthLayout.tsx";
+import Login from "./pages/auth/Login.tsx";
+import Register from "./pages/auth/Registration.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import Dashboard from "./pages/dashboard/Dashboard.tsx";
+import FinalPreview from "./pages/FinalPreview.tsx";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: "/resume/:id",
+    element: <FinalPreview />,
   },
   {
     path: "/resume/edit",
     element: <EditResume />,
   },
   {
-    path: "/resume",
-    element: <MyResume />,
-  },
-  {
     path: "/vacancy",
     element: <JobVacancies />,
+  },
+
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
   },
   {
     path: "/login",
@@ -35,7 +46,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/register",
-    element: <Registration />,
+    element: <Register />,
   },
 ]);
 
@@ -48,13 +59,13 @@ createRoot(document.getElementById("root")!).render(
 );
 
 function AppWrapper() {
-  const [resumeInfo, setResumeInfo] = useState<any>([]);
-
   return (
-    <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </ResumeInfoContext.Provider>
+    <ResumeInfoProvider>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </AuthProvider>
+    </ResumeInfoProvider>
   );
 }
