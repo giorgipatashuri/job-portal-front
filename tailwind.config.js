0a2a61707f89707a517f5bcdc334cc3ee09f5d7e
GIORGI-PC <patashuri2003@gmail.com>
2025-02-02 13:39:38 +0400

    added color hex

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,19 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
-        green: "#4CAF50",
+        green: {
+          DEFAULT: "#4CAF50", // The default shade
+          50: "#E8F5E9", // Lightest shade
+          100: "#C8E6C9",
+          200: "#A5D6A7",
+          300: "#81C784",
+          400: "#66BB6A",
+          500: "#4CAF50", // Your base color
+          600: "#43A047",
+          700: "#388E3C",
+          800: "#2E7D32",
+          900: "#1B5E20", // Darkest shade
+        },
         secGreen: "00E676",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
