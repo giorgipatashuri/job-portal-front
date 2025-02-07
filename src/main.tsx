14205259f4c45ab87890e11dda6a73b6cff027cb
GIORGI-PC <patashuri2003@gmail.com>
2025-02-07 09:42:42 +0400

    added new routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ResumeInfoProvider } from "./context/ResumeInfoContext.tsx";
 import JobVacancies from "./pages/JobVacancies.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Login from "./pages/auth/Login.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import Dashboard from "./pages/dashboard/Dashboard.tsx";
 import FinalPreview from "./pages/FinalPreview.tsx";
 import Register from "./pages/auth/Register.tsx";
 import App from "./App.tsx";
+import CompanyRegister from "./pages/company/Register.tsx";
+import CompanyLogin from "./pages/company/Login.tsx";
+import CompanyDashboardLayout from "./pages/company/dashboard/CompanyDashboardLayout.tsx";
+import ApplicantsList from "./pages/company/components/ApplicantsList.tsx";
+import JobsList from "./pages/company/components/JobsList.tsx";
+import Login from "./pages/auth/Login.tsx";
+import { JobDetailsPage } from "./pages/job/JobDetailsPage.tsx";
+import { JobListingPage } from "./pages/job/JobListingPage.tsx";
 const router = createBrowserRouter([
   {
     path: "/resume/:id",
@@ -31,16 +38,41 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/dashboard",
+    path: "/jobs",
+    element: <JobListingPage />,
+  },
+  {
+    path: "/jobs/:id",
+    element: <JobDetailsPage />,
+  },
+  {
+    path: "/user/dashboard",
     element: <Dashboard />,
   },
   {
-    path: "/login",
+    path: "/user/login",
     element: <Login />,
   },
   {
-    path: "/register",
+    path: "/user/register",
     element: <Register />,
+  },
+  {
+    path: "/company/login",
+    element: <CompanyLogin />,
+  },
+  {
+    path: "/company/register",
+    element: <CompanyRegister />,
+  },
+  {
+    path: "/company/dashboard",
+    element: <CompanyDashboardLayout />,
+    children: [
+      { path: "/company/dashboard/jobs", element: <JobsList /> },
+      { path: "/company/dashboard/applicants", element: <ApplicantsList /> },
+      { path: "/company/dashboard", element: <JobsList /> }, // Default dashboard route
+    ],
   },
 ]);
 
