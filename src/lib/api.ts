2b989cae0a5c3c8f068af36ac770bf9016f93ef3
gpatasha <patashuri2003@gmail.com>
2025-02-08 18:43:00 +0400

    added axios instance for company

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,9 +4,20 @@ const api = axios.create({
   baseURL: "http://localhost:8080/",
   withCredentials: true,
 });
-
+export const apiForCompany = axios.create({
+  baseURL: "http://localhost:8080/",
+  withCredentials: true,
+});
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+apiForCompany.interceptors.request.use((config) => {
+  const token = localStorage.getItem("companyToken");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
